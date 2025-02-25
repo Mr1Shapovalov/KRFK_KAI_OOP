b305c388b4a3ae179a4aa2badea6b2eea05bb7a1
Michailo Shapovalov <mykhailo.sabbath@krfk.kai.edu.ua>
2025-02-25 11:11:56 +0200

    chore added LAB_3

---
+++ src/LAB_3/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LAB_3
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+            Task3();
+        }
+        private static void Task1()
+
+        {
+            int userAge = 20;
+            string userName = "Andrew";
+            Console.WriteLine("Greetings, " + userName + "! Your age: " + userAge);
+        }
+        private static void Task2()
+        {
+            int number = 10;
+            string message = "No";
+            bool isNew = true;
+            Console.WriteLine(number + " " + message + " " + isNew);
+        }
+        private static void Task3()
+        {
+            // Оголошуємо змінну name та присвоюємо їй значення "Анна"
+            string name = "Ana";
+            // Оголошуємо змінну age та присвоюємо їй значення 25
+            int age = 25;
+            // Виводимо на консоль привітання з ім'ям і віком користувача
+            Console.WriteLine("Greetings, " + name + "! Your age: " + age);
+        }
+
+    }
+}
