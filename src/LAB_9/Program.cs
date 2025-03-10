d1a49e272ce3c950269fcee7d3086c73cff1e0b7
Michailo Shapovalov <mykhailo.sabbath@krfk.kai.edu.ua>
2025-03-10 12:03:04 +0200

    chore added LAB_9

---
+++ src/LAB_9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace LAB_9
+{
+    class Program
+    {
+        static void BubbleSort(int[] arr, out int swapCount)
+        {
+            int n = arr.Length;
+            swapCount = 0;
+            bool swapped;
+            for (int i = 0; i < n - 1; i++)
+            {
+                swapped = false;
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1]) 
+                    {
+                        (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                        swapped = true;
+                        swapCount++; 
+                    }
+                }
+                if (!swapped) break; 
+            }
+        }
+        static void Main()
+        {
+            int[] numbers = { 8, 5, 2, 9, 1, 5, 6 };
+            int swapCount;
+            BubbleSort(numbers, out swapCount);
+            Console.WriteLine($"Number of swaps: {swapCount}");
+            Console.WriteLine("After sorting: " + string.Join(", ", numbers));
+        }
+    }
+}
+
