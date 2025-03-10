4ccc797c584134e1efc8a9b9bbb203a1d2a9ab1d
Michailo Shapovalov <mykhailo.sabbath@krfk.kai.edu.ua>
2025-03-10 10:25:55 +0200

    chore added LAB_7

---
+++ src/LAB_7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace LAB_7
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Task 1
+            Console.WriteLine("Even numbers from 2 to 20:");
+            for (int i = 2; i <= 20; i += 2)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n");
+
+            // Task 2
+            int sum = 0;
+            int number;
+            Console.WriteLine("Enter numbers to calculate the sum (0 to terminate):");
+            while ((number = Convert.ToInt32(Console.ReadLine())) != 0)
+            {
+                sum += number;
+            }
+            Console.WriteLine("Sum: " + sum);
+
+            // Task 3
+            string password;
+            do
+            {
+                Console.Write("Enter password: ");
+                password = Console.ReadLine();
+                if (password != "1234")
+                {
+                    Console.WriteLine("Incorrect password!");
+                }
+            } while (password != "1234");
+            Console.WriteLine("Access allowed!");
+        }
+    }
+}
+
